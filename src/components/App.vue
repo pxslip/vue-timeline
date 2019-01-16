e6ec5be704eea44a15f790a5ab706bf539bf23b1
Will Kruse <wskruse@gmail.com>
2019-01-16 16:21:11 -0500

    This is also for the demo...

---
+++ src/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <vue-timeline :slides="slides"></vue-timeline>
+</template>
+
+<script>
+import slides from '../assets/slides';
+export default {
+  data() {
+    return {
+      slides,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
